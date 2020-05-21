004b9a0f5de7e47c550c262aa886e84a5e171f0d
Rajat <tyagirajat200@gmail.com>
2020-05-21 19:15:54 +0530

    c

--- config/dev.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    mongoURI:"mongodb+srv://tyagiapp:tyagiapp@cluster0-3fzth.mongodb.net/test?retryWrites=true&w=majority",
-    IS_PROD: false,
-    COOKIE_NAME:"TYAGI",
-    SESS_SECRET:"MEHUDON"
-}
